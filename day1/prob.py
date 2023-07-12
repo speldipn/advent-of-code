632971dc58b3adccfa3313a227bc96daac72340f
Neo <speldipn@gmail.com>
2023-07-12 18:25:04 +0900

    day1 - prob2 done

--- day1/prob.py
+++ day1/prob.py
@@ -1,4 +1,7 @@
 #!/bin/usr/python3
+from functools import reduce
+
+result = []
 
 f = open("input.txt", "r")
 a = 1
@@ -9,13 +12,19 @@ while True:
   if line == '': 
       break
   elif line == '\n': 
+    result.append(s)
     if s > _max:
       _max = s
     s = 0
   else:
     s += int(line)
+    result.append(int(line))
 
 
-print(_max)
+# problem1
+print("[prob1]", _max)
 
-
+# problem2
+a = sorted(set(result))
+found = a[len(a)-3:len(a)]
+print("[prob2]", found, reduce(lambda found, cur: found + cur, found, 0))
